5d1d0b6252f3010f3fe018609a45c6b8e9045691
Miguel Benitez <benitezhm@gmail.com>
2023-04-19 10:53:03 +0300

    minor fix

--- lib/ivcv_ex.ex
+++ lib/ivcv_ex.ex
@@ -109,28 +109,9 @@ defmodule IvcvEx do
     end
   end
 
-  defp parse_response({:ok, %HTTPoison.Response{status_code: 400, body: body}}) do
-    Logger.error(
-      "#{inspect(__MODULE__)} Server not found, reason: #{inspect(body, pretty: true)}"
-    )
-
-    {:error, "not found"}
-  end
-
-  defp parse_response({:ok, %HTTPoison.Response{status_code: 404, body: body}}) do
-    Logger.error(
-      "#{inspect(__MODULE__)} Server not found, reason: #{inspect(body, pretty: true)}"
-    )
-
-    {:error, "not found"}
-  end
-
-  defp parse_response({:ok, %HTTPoison.Response{status_code: _, body: body}}) do
-    Logger.error(
-      "#{inspect(__MODULE__)} Internal server error, returned: #{inspect(body, pretty: true)}"
-    )
-
-    {:error, "video analysis failed"}
+  defp parse_response({:ok, %HTTPoison.Response{status_code: status_code} = resp}) do
+    Logger.error("Cointraffic responded with code: #{status_code}", response: resp)
+    {:error, "provider_response_status_code_#{status_code}"}
   end
 
   defp parse_response({:error, %HTTPoison.Error{reason: reason}}) do
