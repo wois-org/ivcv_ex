63415a48d86109a3beec6b17cdec4e29c81cb51d
Miguel Benitez <benitezhm@gmail.com>
2023-04-24 16:32:43 +0300

    fix body request and increase receive timeout to 15 seconds

--- lib/ivcv_ex.ex
+++ lib/ivcv_ex.ex
@@ -28,11 +28,63 @@ defmodule IvcvEx do
     http_client = Application.get_env(:ivcv_ex, :http_client)
 
     base_url = Application.get_env(:ivcv_ex, :base_url)
+
+    url = base_url <> path
+
+    body =
+<<<<<<< Updated upstream
+      URI.encode_query(%{
+        "video_url" => video_url
+=======
+      %{"videoUrl" => video_url}
+      |> Jason.encode!()
+
+    headers = [
+      {"Content-Type", "application/json"},
+      {"Authorization", auth_api_key}
+    ]
+
+    http_client.post(url, body, headers, recv_timeout: 15_000)
+    |> parse_response()
+  end
+
+  @doc """
+  Get the result of a video analysis. Result include multiple items from the analysis.
+
+  Returns `{:ok, Result.t()}` and Result contains:
+    emotions (object) - Average intensity and positivity during the video. Integer values between 0 to 100.
+
+    impression (object) - First impression analysis with Big Five metrics. Integer values between 0 to 100.
+
+    sentiment (float) - Sentiment analysis result. Integer value between 0 to 100.
+
+    status (string) - Current status of the processing. One of FINISHED, PROCESSING, FAILED.
+
+    resultId (string) - Id of the result.
+
+  ## Parameters
+
+    - result_id: Id of the result. This is obtained when the video is sent initially.
+
+  ## Examples
+
+      IvcvEx.get_analysis_result("result_id")
+
+  ## Reference
+    https://docs.ivcv.eu/result_for_video.html#blueprints.direct_blueprints.analysis_result
+  """
+  def get_analysis_result(result_id) do
+    path = "/result"
+    auth_api_key = Application.get_env(:ivcv_ex, :auth_key)
+    http_client = Application.get_env(:ivcv_ex, :http_client)
+
+    base_url = Application.get_env(:ivcv_ex, :base_url)
     url = base_url <> path
 
     body =
       URI.encode_query(%{
-        "video_url" => video_url
+        "result_id" => result_id
+>>>>>>> Stashed changes
       })
 
     headers = [
